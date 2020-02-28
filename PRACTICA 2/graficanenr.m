4f2451b3b31e74abf0906383a8063f5b7dcadf96
LeonNavarro <55561931+LeonNavarro@users.noreply.github.com>
2020-02-28 15:22:18 -0600

    Delete graficanenr.m

--- PRACTICA 2/graficanenr.m
+++
@@ -1,7 +0,0 @@
-function graficanenr(dom,funcio)
-%Grafica funciones de N en R con el dominio y la funcion dado
-%   Detailed explanation goes here
-fun= @(n) eval(funcio);
-n=dom;
-Graficarpr6 (n,fun(n));
-end
